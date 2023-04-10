dcc71bf49a06cf89b4bfff3f759803faae2b1507
Oguzhantekin <92325167+Oguzhantekin@users.noreply.github.com>
2023-04-10 15:44:48 +0300

    chat ekranları eklendi

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "assets/AssetManifest.json": "222bc785bdb07236eb8746bafcb02592",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/NOTICES": "1de20cd93e73531153b42d7e73459c57",
+"assets/NOTICES": "19db53259a6c4b2f5581421a3776eb6d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
@@ -20,10 +20,10 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "16ff1adccd666ffdfde7076681a86a79",
-"/": "16ff1adccd666ffdfde7076681a86a79",
-"main.dart.js": "168245e5893e3a1ffb453f39baa60516",
-"manifest.json": "d865732d97de6bc68ec8c6994fd99995",
+"index.html": "920bb917d1e03bb5eeb9af909071a1ec",
+"/": "920bb917d1e03bb5eeb9af909071a1ec",
+"main.dart.js": "840e5bebdc521db26d15aabf8fcaa6c8",
+"manifest.json": "778bb2e7436817972e8ee21fabf9d51e",
 "version.json": "04a0d365a069df6796259842abcf05e0"
 };
 
